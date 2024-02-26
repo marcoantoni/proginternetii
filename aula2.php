e35183632ce30064ba5aa9e2a3d392934b90fb51
Marco Antoni <marco.antoni910@gmail.com>
2024-02-26 07:58:35 -0300

    Introdução ao PHP

--- aula2.php
+++ aula2.php
@@ -1,11 +1,13 @@
 <?php
-	$idade = 20;
+	$idade = 18;
 
-	$passou_no_teste = true; // true -> aprovado falase -> reprovado
+	$passou_no_teste = false; // true -> aprovado falase -> reprovado
 
 	// exemplo de if com duas condições
 	if ($idade >= 18 && $passou_no_teste == true)
 		echo ("Apto a tirar a CNH");
+	else if ($idade < 18)
+		echo ("Para estar apto, é preciso ser maior de idade");
 	else
-		echo ("Não a apto a tirar a CNH");
+		echo ("Para estar apto, é preciso passar no teste psicológico");
 ?>
