cd3ead5f96e6434289c651c3d0bfa96b7937c297
Marco Antoni <marco.antoni910@gmail.com>
2024-02-26 07:46:40 -0300

    Introdução ao PHP
    if com duas condições

---
+++ aula2.php
@@ -0,0 +1,11 @@
+<?php
+	$idade = 20;
+
+	$passou_no_teste = true; // true -> aprovado falase -> reprovado
+
+	// exemplo de if com duas condições
+	if ($idade >= 18 && $passou_no_teste == true)
+		echo ("Apto a tirar a CNH");
+	else
+		echo ("Não a apto a tirar a CNH");
+?>
