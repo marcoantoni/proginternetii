dd13358750d05c4870e25d2a551da7f17092d84f
Marco Antoni <marco.antoni910@gmail.com>
2024-04-16 07:50:16 -0300

    Formulário enviar mensagem

---
+++ enviar_mensagem.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Formulário de envio de mensagem</title>
+</head>
+<body>
+	<form>
+		<fieldset>
+			<legend>Enviando uma mensagem</legend>
+			Nome <input type="text"> <br>
+			Nascimento: <input type="date"> <br>
+			CPF <input type="text"> <br>
+			Telefone <input type="tel"> <br>
+			Email <input type="email"> <br>
+			Sexo <br> 
+			<input type="radio" name="sexo"> Feminino <input type="radio" name="sexo">Masculino <input type="radio" name="sexo"> Intersexo <br>
+			Qual o melhor dia e horário para entrar em contato? <br>
+			Horário <input type="time"> <br>
+			Dia da semana <br>
+			<input type="checkbox"> Segunda-feira 
+			<input type="checkbox"> Terça-feira 
+			<input type="checkbox"> Quarta-feira 
+			<input type="checkbox"> Quinta-feira 
+			<input type="checkbox"> Sexta-feira 
+			<input type="checkbox"> Sabádo 
+			<br>
+			Qual o seu problema?<br>
+			<textarea cols="80" rows="5"></textarea> <br>
+			<button>Enviar</button>
+		</fieldset>
+	</form>
+</body>
+</html>
