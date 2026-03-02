dce2ab52cf0eef3f16e534b72949de4104312f9d
marco <marco.antoni910@gmail.com>
2026-03-02 09:07:12 -0300

    Correçãoe exercicio 1 - Aula 1

---
+++ www/ex_est_decisao.php
@@ -0,0 +1,12 @@
+<?php
+	// ex_est_decisao.php
+	
+	// correção exercícios aula 1
+
+	$presencas = 90;	// armazenando a quantidade de aulas que o aluno veio
+
+	$freq = ($presencas / 120) * 100;
+
+	echo ("O estudante que assistiu $presencas aulas tem frequencia de $freq %");
+
+?>
